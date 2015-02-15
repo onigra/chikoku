3954c8a9dda49d550bc539741377f162c1ba0257
onigra <3280467rec@gmail.com>
2015-02-15 21:49:32 +0900

    twitter oauthの雑な実装

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module ChikokuNotification
     # generators
     config.generators do |g|
       g.test_framework :rspec
+      g.fixture_replacement :factory_girl
       g.view_specs false
     end
 
