a3a9e7ef7a39438844485bed2a99a0554c540259
onigra <3280467rec@gmail.com>
2015-02-17 21:04:26 +0900

    勤務地登録のルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   root 'home#index'
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/logout' => 'sessions#destroy', as: :logout
+
+  get   '/work_places/new',  to: 'work_places#new'
+  post  '/work_places',      to: 'work_places#create'
+  get   '/work_places/edit', to: 'work_places#edit'
+  patch '/work_places',      to: 'work_places#update'
+  put   '/work_places',      to: 'work_places#update'
 end
